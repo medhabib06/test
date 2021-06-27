3284abc02c4f00599edea9dcd7cedef17d1ca5c3
medhabib06 <medhabib06@gmail.com>
2021-06-28 00:25:21 +0100

    7th

--- src/App.js
+++ src/App.js
@@ -4,11 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        jhghjghjghjddd
-      fgfggg
-        fff
-        fff
-        ff
+        aaa
     </div>
   );
 }
